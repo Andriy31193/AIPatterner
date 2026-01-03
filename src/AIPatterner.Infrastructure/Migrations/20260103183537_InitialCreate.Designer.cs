d36f648e86fc7db85168f000d2cae0581129bf88
Andriy31193 <andriycreator@gmail.com>
2026-01-03 19:32:04 +0000

    Fixing matching patterns and tests.

--- src/AIPatterner.Infrastructure/Migrations/20260103183537_InitialCreate.Designer.cs
+++ src/AIPatterner.Infrastructure/Migrations/20260103183537_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace AIPatterner.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20260103175044_InitialCreate")]
+    [Migration("20260103183537_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
